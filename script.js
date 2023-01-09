a94017934c739981ce855a0a221c49d4fe23b57a
RahulAgnihotri1998 <rahulagnihotri1998@gmail.com>
2023-01-09 16:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-   
-const addButton = document.getElementById('add');
 const itemNameInput = document.getElementById('item-name-input');
 const itemPriceInput = document.getElementById('item-price-input');
-const totalElement = document.getElementById('total');
+const addButton = document.getElementById('add');
+const itemsTable = document.getElementById('items');
+const total = document.getElementById('total');
 
-let total = 0;
+let grandTotal = 0;
 
 addButton.addEventListener('click', () => {
+  // Get the input values
   const itemName = itemNameInput.value;
   const itemPrice = itemPriceInput.value;
 
-  // Add a new row to the table with the item name and price
-  const tableBody = document.querySelector('tbody');
-  const newRow = document.createElement('tr');
-  newRow.innerHTML = `<td>${itemName}</td><td>${itemPrice}</td>`;
-  tableBody.appendChild(newRow);
+  // Validate input
+  if (itemName === '' || itemPrice === '' || isNaN(itemPrice)) {
+    return;
+  }
 
   // Update the grand total
-  total += parseFloat(itemPrice);
-  totalElement.textContent = total;
+  grandTotal += parseFloat(itemPrice);
 
   // Clear the input fields
   itemNameInput.value = '';
   itemPriceInput.value = '';
+
+  // Add a new row to the table
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+    <td aria-role="gridcell">${itemName}</td>
+    <td aria-role="gridcell">${itemPrice}</td>
+  `;
+  itemsTable.insertBefore(newRow, total.parentElement);
+
+  // Update the grand total display
+  total.textContent = grandTotal.toFixed(2);
 });
