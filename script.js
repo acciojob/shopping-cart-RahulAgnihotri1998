763111feb6af5b8e36b752236f66f316776b6b53
RahulAgnihotri1998 <rahulagnihotri1998@gmail.com>
2023-01-09 16:21:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-//your code here
-const shoppingList = {
-    itemNameInput: document.querySelector('#item-name-input'),
-    itemPriceInput: document.querySelector('#item-price-input'),
-    addButton: document.querySelector('#add'),
-    shoppingTable: document.querySelector('table'),
-    total: document.querySelector('#total'),
-  };
-  
-  shoppingList.addButton.addEventListener('click', () => {
-    const itemName = shoppingList.itemNameInput.value;
-    const itemPrice = parseFloat(shoppingList.itemPriceInput.value);
-  
-    if (itemName === '' || isNaN(itemPrice) || itemPrice < 0) {
-      return;
-    }
-  
-    const row = document.createElement('tr');
-    const itemColumn = document.createElement('td');
-    itemColumn.textContent = itemName;
-    const priceColumn = document.createElement('td');
-    priceColumn.textContent = itemPrice;
-  
-    row.appendChild(itemColumn);
-    row.appendChild(priceColumn);
-    shoppingList.shoppingTable.lastElementChild.prepend(row);
-  
-    shoppingList.total.textContent = (parseFloat(shoppingList.total.textContent) + itemPrice).toFixed(2);
-  
-    shoppingList.itemNameInput.value = '';
-    shoppingList.itemPriceInput.value = '';
-  });
-  
+      const itemInput = document.getElementById("item-name-input");
+      const priceInput = document.getElementById("item-price-input");
+      const addButton = document.getElementById("add");
+      const shoppingList = document.getElementById("shopping-list");
+      const grandTotal = document.getElementById("grand-total");
+      const totalLabel = document.getElementById("total");
+
+      addButton.addEventListener("click", () => {
+        const itemName = itemInput.value;
+        const itemPrice = priceInput.value;
+
+        if (!itemName || !itemPrice) {
+          alert("Please enter a valid item name and price.");
+          return;
+        }
+
+        const row = document.createElement("tr");
+        const itemColumn = document.createElement("td");
+        const priceColumn = document.createElement("td");
+
+        itemColumn.innerText = itemName;
+        priceColumn.innerText = "$" + itemPrice;
+
+        row.appendChild(itemColumn);
+        row.appendChild(priceColumn);
+
+        shoppingList.insertBefore(row, grandTotal.parentElement.parentElement);
+
+        const currentTotal = parseFloat(grandTotal.innerText.substring(1));
+        grandTotal.innerText = "$" + (currentTotal + parseFloat(itemPrice)).toFixed(2);
+        totalLabel.innerText = "Grand Total: " + grandTotal.innerText;
+
+        itemInput.value = "";
+        priceInput.value = "";
+      });
